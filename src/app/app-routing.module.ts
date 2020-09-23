a453d10d23fcc96a6c106b275d831f5d2ff8ba50
kaito_ohishi <kaito990626@yahoo.o.jp+ohishikaito@users.noreply.github.com>
2020-09-23 14:26:13 +0900

    アカウントのルーター導入まで

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ChatComponent } from './chat/chat.component';
 import { PageNotFoundComponent } from './error/page-not-found/page-not-found.component';
+import { AccountModule } from './account/account.module';
 
 const routes: Routes = [
   { path: 'test', redirectTo: '', pathMatch: 'full' },
+  {
+    path: 'account',
+    loadChildren: () =>
+      import('./account/account.module').then((m) => m.AccountModule),
+  },
   {
     path: '',
     component: ChatComponent,
@@ -13,7 +19,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), AccountModule],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
